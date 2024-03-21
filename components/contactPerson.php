bcf3f165017c2ff336f1bccafe2f2afbba86d2ce
Fogado Medhane Semena <fog0408>
2024-03-21 09:02:13 +0100

    nre method to render user and filteruser

--- components/contactPerson.php
+++ components/contactPerson.php
@@ -1,4 +1,5 @@
 <?php
+
     class ContactPerson {
         public string $email;
         public string $phoneNumber;
@@ -6,7 +7,8 @@
         public string $position;
         public int $comapnyId;
 
-        public function __construct(string $email, string $phoneNumber, string $name, string $position, int $comapnyId){
+        public function __construct(int $contectPersonId, string $email, string $phoneNumber, string $name, string $position, int $comapnyId){
+            $this -> contectPersonId = $contectPersonId;
             $this -> email = $email;
             $this -> phoneNumber = $phoneNumber;
             $this -> name = $name;
@@ -14,14 +16,48 @@
             $this -> comapnyId = $comapnyId;
         }
 
-        public function getArray(): array {
-            return [
-                "email" => $this->email,
-                "phonenumber" => $this->phoneNumber,
-                "name" => $this->name,
-                "position" => $this->position,
-                "lærlingsbedrift_ID" => $this->companyId,
-            ];
+        public function render($filterCompany = ""){
+
+            if($filterCompany != ""){
+                if($filterCompany == $this -> getCompanyName($this -> comapnyId)){
+                    echo "<tr>";
+                    echo "<td>".$this -> contectPersonId."</td>"; 
+                    echo "<td>".$this -> email."</td>"; 
+                    echo "<td>".$this -> phoneNumber."</td>"; 
+                    echo "<td>".$this -> name."</td>"; 
+                    echo "<td>".$this -> position."</td>"; 
+                    echo "<td>".$this -> comapnyId."</td>"; 
+        
+                    echo "<td>".contactDeleteForm($this -> contectPersonId);
+                    echo contactUpdateForm($this -> contectPersonId)."</td>";
+        
+                    echo "</tr>";
+                    return;
+                };
+                return;
+            };
+        }
+
+        private function getCompanyName($id){
+            require "connection.php";
+
+            $query = "SELECT * FROM apprenticecompany WHERE id = $id";
+            $result = $connection -> query($query);
+            
+
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                
+                $companyName = $row['Name'];
+                
+                $result->free_result();
+                
+                $connection->close();
+                
+                return strtolower($companyName);
+            } else {
+                return "";
+            }
         }
     }
 ?>
