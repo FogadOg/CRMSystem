3284a8094a883178ca69269f263845cc173f08b0
Fogado Medhane Semena <fog0408>
2024-03-22 08:54:53 +0100

    adding styiling

--- components/contactPerson.php
+++ components/contactPerson.php
@@ -1,3 +1,19 @@
+<style>
+    
+    .crudButton{
+        background-color: transparent;
+        border: none;
+        font-weight: 600; 
+        margin-top: .1rem;
+        transition: 100ms;
+    }
+
+    .crudButton:hover{
+        transform: scale(1.1);
+    }
+
+</style>
+
 <?php
 
     class ContactPerson {
