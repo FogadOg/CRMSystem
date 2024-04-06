a324a58c436b8014c813d4b56266099a1b3e9509
FarouqJalabi <139611840+FarouqJalabi@users.noreply.github.com>
2024-04-06 19:45:56 +0200

    Changing color if selected

--- components/company.php
+++ components/company.php
@@ -31,10 +31,12 @@ class Company {
             window.location.search = urlParams;
         }
         </script>';
+        $selected = isset($_GET['company']) && $_GET['company'] == $this->companyId;
+        $color = $selected ? "danger" : "primary";
         echo "<tr>";
         echo "<td>".$this->companyId."</td>"; 
         echo "<td>".$this->companyName."</td>"; 
-        echo "<td><button class='btn btn-primary' id='button_".$this->companyId."' onclick='toggleFilter(\"".$this->companyId."\")'>Toggle</button></td>";
+        echo "<td><button class='btn btn-".$color."' id='button_".$this->companyId."' onclick='toggleFilter(\"".$this->companyId."\")'>Toggle</button></td>";
         echo "<td>".deleteForm($this->companyId);
         echo updateForm($this->companyId)."</td>";
         echo "</tr>";
